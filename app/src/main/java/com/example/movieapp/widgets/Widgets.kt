a875c751959942ad50cd3a31649dafc455ea452f
dominik.listos <dominik.listos@mobica.com>
2022-12-15 14:19:12 +0100

    Expandable items and spannable text

--- app/src/main/java/com/example/movieapp/widgets/Widgets.kt
+++ app/src/main/java/com/example/movieapp/widgets/Widgets.kt
@@ -1,5 +1,6 @@
 package com.example.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -9,19 +10,33 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
 import com.example.movieapp.data.Movie
@@ -30,11 +45,14 @@ import com.example.movieapp.data.getMovies
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies().first(), onItemClick: (String) -> Unit = {}) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(120.dp)
+            .wrapContentHeight()
             .clickable {
                 onItemClick.invoke(movie.id)
             },
@@ -68,19 +86,59 @@ fun MovieRow(movie: Movie = getMovies().first(), onItemClick: (String) -> Unit =
             Column(modifier = Modifier.padding(4.dp)) {
                 Text(
                     text = movie.title,
-                    modifier = Modifier.padding(horizontal = 5.dp),
                     style = MaterialTheme.typography.h6
                 )
                 Text(
                     text = "Director: ${movie.director}",
-                    modifier = Modifier.padding(horizontal = 5.dp),
                     style = MaterialTheme.typography.caption
                 )
                 Text(
                     text = "Released: ${movie.year}",
-                    modifier = Modifier.padding(horizontal = 5.dp),
                     style = MaterialTheme.typography.caption
                 )
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+
+                        Text(
+                            buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 13.sp
+                                    )
+                                ) { append("Plot: ") }
+
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 13.sp,
+                                        fontWeight = FontWeight.Light
+                                    )
+                                ) { append(movie.plot) }
+                            },
+                            modifier = Modifier.padding(horizontal = 6.dp)
+                        )
+
+                        Divider()
+
+                        Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.caption)
+                        Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.caption)
+                        Text(text = "Rating: ${movie.rating}", style = MaterialTheme.typography.caption)
+
+                    }
+                }
+
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = expanded.not()
+                        },
+                    tint = Color.DarkGray
+                )
+
             }
 
 
